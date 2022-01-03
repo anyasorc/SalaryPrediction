6d0f21ae705652639f7910fccd94150860e67c69
anyasorc <31804838+anyasorc@users.noreply.github.com>
2022-01-03 12:42:44 +0100

    first commit

---
+++ model/model.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from sklearn import metrics
+import pickle
+
+
+ds = pd.read_csv('/Users/anyasorc/python/data/Salary_Data.csv')
+print(ds.shape)
+X = ds.iloc[:, 0:1].values
+y = ds.iloc[:, 1].values
+
+#split the data into train and test dataset of 80 for train and 20 for test
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
+
+
+LR = LinearRegression()
+#trained iwth X_train and y_train data
+LR.fit(X_train, y_train)
+
+predicted = LR.predict(X_test)
+
+
+#save the trained model to disk
+pickle.dump(LR, open('model.pkl', 'wb'))
+
---
+++ server/server.py
@@ -0,0 +1,21 @@
+import numpy as np
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/api/getsal', methods=['POST'])
+def predict():
+    # Get the data from the POST request.
+    data = request.get_json(force=True)
+    # Make prediction using model loaded from disk as per the data.
+    prediction = model.predict([[np.array(data['exp'])]])
+    # Take the first value of prediction
+    output = prediction[0]
+    return jsonify(predictedvlue = output)
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
